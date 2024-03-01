727fc3515ab08ea89f4bf8774500ae6cf4b2b4e2
Dasajton <david.sajitz.94@gmail.com>
2024-03-01 22:35:17 +0100

    firm with empty values

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom";
+import {
+	createBrowserRouter,
+	Navigate,
+	RouterProvider,
+} from "react-router-dom";
 import "./index.scss";
 import { PageSimpleForm } from "./pages/PageSimpleForm.tsx";
-import { PageAbout } from "./pages/PageAbout.tsx";
+import { PageStateForm } from "./pages/PageStateForm.tsx";
 import { Page404 } from "./pages/Page404.tsx";
 import { PageEmployees } from "./pages/PageEmployees.tsx";
-import * as config from './config';
+import * as config from "./config";
 
 const router = createBrowserRouter([
 	{
@@ -16,15 +20,20 @@ const router = createBrowserRouter([
 		children: [
 			{
 				path: "/simple-form",
-				element: config.environment === 'development' ? <PageSimpleForm /> : <Navigate to="/employees" replace />
+				element:
+					config.environment === "development" ? (
+						<PageSimpleForm />
+					) : (
+						<Navigate to="/employees" replace />
+					),
 			},
 			{
 				path: "/employees",
 				element: <PageEmployees />,
 			},
 			{
-				path: "/about",
-				element: <PageAbout />,
+				path: "/state-form",
+				element: <PageStateForm />,
 			},
 			{
 				path: "/",
